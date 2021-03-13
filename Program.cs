3ba6c75df4fad3a3ef4fa52dafce754977fc6a6c
gabriel-slovak <slovakgabriellopes@gmail.com>
2021-03-13 13:50:52 -0300

    finalização do programa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase; 
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("----------");
+            Console.WriteLine("Cebolinha");
+            Console.WriteLine("----------");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.Write("Digite uma frase que tenha 'R': ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+            Console.ResetColor();
+            Console.WriteLine();
+
+        }
+    }
+}
